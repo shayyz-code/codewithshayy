46c2c8c8439ccf8ab8a8a6012a730b7468e96ef2
shayyz-code <aungminkhant.shay@gmail.com>
2025-02-16 21:17:29 +0630

    Update developer photo

--- src/backend/addEnrolled.ts
+++ src/backend/addEnrolled.ts
@@ -1,21 +1,21 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "@/backend/firebase";
-import { TSelectItem } from "@/ui/SelectInput";
+import { addDoc, collection } from "firebase/firestore"
+import { db } from "@/backend/firebase"
+import { TSelectItem } from "@/ui/SelectInput"
 
 type TAddEnrolled = {
-  name: string;
-  email: string;
-  course: TSelectItem;
-};
+  name: string
+  email: string
+  course: TSelectItem
+}
 
 export default async function addEnrolled(data: TAddEnrolled) {
   try {
-    const resEnrolled = await addDoc(collection(db, "enrolled"), data);
-    const docId = resEnrolled.id;
-    console.log(resEnrolled);
-    return { docId: docId, status: "ok" };
+    const resEnrolled = await addDoc(collection(db, "enrolled"), data)
+    const docId = resEnrolled.id
+    console.log(resEnrolled)
+    return { docId: docId, status: "ok" }
   } catch (error) {
-    console.log(error);
-    return { docId: "", status: "fail" };
+    console.log(error)
+    return { docId: "", status: "fail" }
   }
 }
